6bd0869d29eb4b8e164625bfb19710200bf2967a
ahmadabobakr <ahmadabobakr@gmail.com>
2019-07-19 19:29:49 +0400

    added a grabable object editor

--- Assets/Kandooz/KVR/Scripts/MonoBehaviours/Editor/Grabable.cs
+++ Assets/Kandooz/KVR/Scripts/MonoBehaviours/Editor/Grabable.cs
@@ -7,28 +7,101 @@ namespace Kandooz.KVR {
     public class GrabableEditor : Editor
     {
         Grabable grabable;
+        Transform leftPivot, rightPivot;
+        HandAnimationController currentlyControlledHand;
+        Hand hand;
+        bool editmode;
         void OnEnable()
         {
             grabable = (Grabable)target;
+            grabable. Initialize();
+            editmode = false;
+            serializedObject.FindProperty("editMode").boolValue = false; ;
+
+            leftPivot = (Transform)serializedObject.FindProperty("leftPivot").objectReferenceValue;
+            rightPivot= (Transform)serializedObject.FindProperty("rightPivot").objectReferenceValue;
+            
         }
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
-            var editmode =serializedObject.FindProperty("editMode").boolValue;
-            var pivot = (GameObject)serializedObject.FindProperty("handPosition").objectReferenceValue;
-            var child = pivot.transform.Find("hand");
-
-            if (editmode)
+            if (serializedObject.FindProperty("editMode").boolValue)
             {
-                if (!child)
+                var hand = serializedObject.FindProperty("handToEdit");
+                EditorGUILayout.PropertyField(hand);
+                Debug.Log(hand.enumValueIndex );
+                serializedObject.ApplyModifiedProperties();
+                if (hand.enumValueIndex != (int)this.hand ||!currentlyControlledHand)
                 {
-                    GameObject.Instantiate(grabable.data.HandPrefab,pivot.transform);
+                    this.hand = (Hand)hand.enumValueIndex;
+                    if (currentlyControlledHand) 
+                        GameObject.DestroyImmediate(currentlyControlledHand.gameObject);
+                    Debug.Log(this.hand.ToString());
+                    switch (this.hand)
+                    {
+                        case Hand.right:
+                            currentlyControlledHand = GameObject.Instantiate(grabable.data.rightHandPrefab,Vector3.zero,Quaternion.identity,rightPivot);
+                            currentlyControlledHand.transform.localPosition = Vector3.zero;
+                            currentlyControlledHand.transform.localRotation= Quaternion.identity;
+                            break;
+                        case Hand.left:
+                            currentlyControlledHand = GameObject.Instantiate(grabable.data.leftHandPrefab, Vector3.zero, Quaternion.identity, leftPivot);
+                            currentlyControlledHand.transform.localPosition = Vector3.zero;
+                            currentlyControlledHand.transform.localRotation = Quaternion.identity;
+
+                            break;
+                    }
+                    currentlyControlledHand.name = "hand";
                 }
+                SerializedObject so = new SerializedObject(currentlyControlledHand);
+                HandAnimationControllerEditor.DisplayHandEditor(currentlyControlledHand,so);
+
             }
             else
             {
-
+                if (currentlyControlledHand)
+                    GameObject.DestroyImmediate(currentlyControlledHand.gameObject);
+                currentlyControlledHand = null;
             }
+        }
+        void OnDisable()
+        {
+            if (currentlyControlledHand)
+                GameObject.DestroyImmediate(currentlyControlledHand.gameObject);
+            currentlyControlledHand = null;
+            Tools.hidden = false;
+
+        }
+        void OnSceneGUI()
+        {
+            if (currentlyControlledHand)
+            {
+                Tools.hidden = true;
+                var parent = currentlyControlledHand.transform.parent;
+                EditorGUI.BeginChangeCheck();
+                var deltaRotation = Handles.DoRotationHandle(parent.localRotation, parent.position);
+                Debug.Log(deltaRotation.eulerAngles);
+                parent.localRotation = deltaRotation;
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RegisterFullObjectHierarchyUndo(parent, "Rotated " + parent.name);
+                }
+
+                EditorGUI.BeginChangeCheck();
+                var deltaPosition = Handles.DoPositionHandle(parent.position,parent.rotation);
+                
+                parent.position= deltaPosition;
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RegisterFullObjectHierarchyUndo(parent, "moved" + parent.name);
+                }
+                
+            }
+            else
+            {
+                Tools.hidden = false;
+            }
+
         }
     }
 }
--- Assets/Kandooz/KVR/Scripts/MonoBehaviours/Grabable.cs
+++ Assets/Kandooz/KVR/Scripts/MonoBehaviours/Grabable.cs
@@ -4,16 +4,59 @@ using UnityEngine;
 
 namespace Kandooz.KVR
 {
+    public enum Hand
+    {
+        right,left
+    }
     public class Grabable : MonoBehaviour
     {
         
         public HandData data;
         [SerializeField] private bool editMode;
-        [SerializeField] [HideInInspector] private GameObject handPosition;
+        [SerializeField] [HideInInspector] private Hand handToEdit;
 
+        [SerializeField] [HideInInspector] private Transform leftPivot;
+        [SerializeField] [HideInInspector] private Transform rightPivot;
+
+        public void Initialize()
+        {
+            if (!leftPivot)
+            {
+                leftPivot = transform.Find("leftPivot");
+                if (!leftPivot)
+                {
+                    leftPivot = new GameObject().transform;
+                    leftPivot.parent = this.transform;
+                    leftPivot.transform.position = Vector3.zero;
+                    leftPivot.transform.rotation = Quaternion.identity;
+                    leftPivot.name = "leftPivot";
+                }
+            }
+            else
+            {
+                leftPivot.name = "leftPivot";
+            }
+            if (!rightPivot)
+            {
+                rightPivot = transform.Find("rightPivot");
+                if (!rightPivot)
+                {
+                    rightPivot = new GameObject().transform;
+                    rightPivot.parent = this.transform;
+                    rightPivot.transform.position = Vector3.zero;
+                    rightPivot.transform.rotation = Quaternion.identity;
+                    rightPivot.name = "rightPivot";
+                }
+            }
+            else
+            {
+                rightPivot.name = "rightPivot";
+
+            }
+        }
         public void OnGrabStart()
         {
-
+            
         }
         public void OnGrabEnd()
         {
