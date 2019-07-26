2312a6e83dea33a06f8953ffdc7441c552722336
ahmadabobakr <ahmadabobakr@gmail.com>
2019-07-27 00:31:05 +0400

    fixed animation controller bug

--- Assets/Kandooz/KVR/Scripts/MonoBehaviours/HandController.cs
+++ Assets/Kandooz/KVR/Scripts/MonoBehaviours/HandController.cs
@@ -17,6 +17,7 @@ namespace Kandooz.KVR
         {
             type = GetComponent<HandTracker>().type;
             collider = new Collider[1];
+            animationController=GetComponentInChildren<HandAnimationController>();
         }
         private void Update()
         {
@@ -24,12 +25,12 @@ namespace Kandooz.KVR
             {
                 animationController[i]=inputManager.GetFingerValue(type, (FingerName)i);
             }
-            Physics.OverlapSphereNonAlloc(this.transform.position + shift, radius, collider);
-            if (collider[0])
-            {
-                var interactable = collider[0].GetComponentInParent<Interactable>();
-                interactable.OnHandHover(this);
-            }
+            // Physics.OverlapSphereNonAlloc(this.transform.position + shift, radius, collider);
+            // if (collider[0])
+            // {
+            //     var interactable = collider[0].GetComponentInParent<Interactable>();
+            //     interactable.OnHandHover(this);
+            // }
         }
         void OnDrawGizmosSelected()
         {
