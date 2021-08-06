a1f3923060a4d51662907a1882ec679d3786cb22
ahmadabobakr <ahmadabobakr@gmail.com>
2021-08-06 21:48:00 +0400

    Started adding the new Pose system

---
+++ Assets/Kandooz/KVR/Scripts/Classes/IPose.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kandooz.KVR
+{
+    interface IPose
+    {
+        float this[int indexer]{set;}
+        float Weight { get; set; }
+    }
+}
---
+++ Assets/Kandooz/KVR/Scripts/Classes/StaticPose.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kandooz.KVR
+{
+    public class StaticPose : IPose
+    {
+        public float this[int indexer] { set => throw new NotImplementedException(); }
+
+        public float Weight { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+
+}
