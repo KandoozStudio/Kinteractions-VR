942994ff15556322ef9f2ababcec2ce7342a4f0c
ahmadabobakr <ahmadabobakr@gmail.com>
2021-08-07 15:33:19 +0400

    Fixed merge issues

---
+++ Assets/Kandooz/KVR/Scripts/CustomEvents/HandEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Kandooz.KVR
+{
+    [System.Serializable]
+    public class HandEvent : UnityEvent<Hand>
+    {
+    }
+
+}
