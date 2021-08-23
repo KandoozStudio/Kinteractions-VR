037b1a4363b845dea098850f5a0d258adf6c9c28
ahmad abobakr <ahmadabobakr@gmail.com>
2021-08-23 23:21:57 +0400

    Interactor now selects the closest interactable

--- Assets/Kandooz/KVR/Scripts/MonoBehaviours/Interactor.cs
+++ Assets/Kandooz/KVR/Scripts/MonoBehaviours/Interactor.cs
@@ -6,18 +6,17 @@ using UnityEngine;
 namespace Kandooz.KVR
 {
     [RequireComponent(typeof(HandInputMapper))]
+    [RequireComponent(typeof(Collider))]    
     public class Interactor : MonoBehaviour
     {
         HandInputMapper mapper;
-        [ReadOnly] Interactable currentInteractale;
-        [ReadOnly] Collider currentCollider;
-        [ReadOnly] bool interacting;
-
+        [ReadOnly][SerializeField] Interactable currentInteractale;
+        [ReadOnly][SerializeField] Collider currentCollider;
+        [ReadOnly][SerializeField] bool interacting;
         private void Awake()
         {
             mapper = GetComponent<HandInputMapper>();
         }
-
         private void OnTriggerEnter(Collider other)
         {
             if (interacting)
@@ -26,18 +25,48 @@ namespace Kandooz.KVR
             }
             if (currentCollider != other)
             {
-                currentCollider = other;
+                CheckInteractablevalididty(other);
             }
         }
+        private void CheckInteractablevalididty(Collider other)
+        {
+            var interactable = other.GetComponentInParent<Interactable>();
+            if (interactable)
+            {
+
+                if (IsColliderCloserThanCurrent(other))
+                {
+                    currentCollider = other;
+                    currentInteractale = interactable;
+                }
+            }
+        }
+        private bool IsColliderCloserThanCurrent(Collider collider)
+        {
+            if (!currentCollider)
+            {
+                return true;
+            }
+            var distanceTocurrentInteractor = (currentCollider.transform.position - this.transform.position).magnitude;
+            var distanceToNewInteractor = (collider.transform.position - this.transform.position).magnitude;
+            if (distanceTocurrentInteractor > distanceToNewInteractor)
+            {
+                
+                return true;
+            }
+            return false;
+        }
+
         private void OnTriggerExit(Collider other)
         {
             if (interacting)
             {
                 return;
             }
-            if (currentCollider==other)
+            if (currentCollider == other)
             {
                 currentCollider = null;
+                currentInteractale = null;
             }
         }
 
