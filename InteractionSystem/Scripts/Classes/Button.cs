49fa282181bd9178064f25fe5227f4de4eb22c24
Ahmad abobakr <AhmadaboBakr@gmail.com>
2021-10-04 23:28:19 +0400

    Changed the Button Enumto map to common usages

--- InteractionSystem/Scripts/Classes/Button.cs
+++ InteractionSystem/Scripts/Classes/Button.cs
@@ -8,14 +8,16 @@ namespace Kandooz.KVR
 {
     public enum Button
     {
-        Trigger,
-        Grip,
-        Primary,
-        Secondry,
-        PrimaryTouch,
-        SecondryTouch,
-        Menu,
-        AxisTouch,
-        AxisClick
+        TriggerButton = 0,
+        GripButton = 1,
+        PrimaryButton = 2,
+        SecondaryButton = 3,
+        PrimaryTouch = 4,
+        SecondryTouch = 5,
+        MenuButton = 6,
+        primary2DAxisTouch = 7,
+        SecondaryTouch = 8,
+        Primary2DAxisClick = 9,
+        Secondary2DAxisClick = 10,
     }
 }
