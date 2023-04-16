b24439646ba97b79809dbb95c1ddf5a5c8eaf8b4
Ahmad Abobakr <Ahmadabobakr@gmail.com>
2023-04-17 01:03:13 +0400

    Config file now contains the layers for left and right hand

--- Interactions/Runtime/Core/Config.cs
+++ Interactions/Runtime/Core/Config.cs
@@ -6,6 +6,49 @@ namespace Kandooz.Interactions.Runtime
     [CreateAssetMenu]
     public class Config : ScriptableObject
     {
+        [FormerlySerializedAs("inputManagerBase")] [SerializeField] private InputManagerBase inputManager;
+        [SerializeField] private LayerMask leftHandLayer;
+        [SerializeField] private LayerMask rightHandLayer;
+        [SerializeField] private LayerMask interactableLayer;
+        [SerializeField] private GameObject gameManager;
+        [SerializeField] private InputManagerType inputType;
+        public int LeftHandLayer => leftHandLayer;
+        public int RightHandLayer => leftHandLayer;
+        public InputManagerBase InputManager
+        {
+            get
+            {
+                if (inputManager) return inputManager;
+                if (gameManager) return CreateInputManager();
+                gameManager = new GameObject("VR Manager");
+                DontDestroyOnLoad(gameManager);
+                return CreateInputManager();
+            }
+        }
+        private InputManagerBase CreateInputManager()
+        {
+            switch (inputType)
+            {
+                case InputManagerType.UnityAxisBased:
+                    if (inputManager != null && inputManager is AxisBasedInputManagerBase) return inputManager;
+                    if (inputManager) Destroy(inputManager);
+                    inputManager = gameManager.AddComponent<AxisBasedInputManagerBase>();
+                    break;
+                case InputManagerType.InputSystem:
+                    if (inputManager != null && inputManager is InputSystemBased) return inputManager;
+                    if (inputManager) Destroy(inputManager);
+                    inputManager = gameManager.AddComponent<InputSystemBased>();
+                    break;
+                case InputManagerType.KeyboardMock:
+                    if (inputManager != null && inputManager is KeyboardBasedInput) return inputManager;
+                    if (inputManager) Destroy(inputManager);
+                    inputManager = gameManager.AddComponent<KeyboardBasedInput>();
+
+                    break;
+            }
+
+            return inputManager;
+        }
         private enum InputManagerType
         {
             UnityAxisBased = 0,
@@ -13,50 +56,6 @@ namespace Kandooz.Interactions.Runtime
             KeyboardMock = -1
         }
 
-        [FormerlySerializedAs("inputManager")] [SerializeField]
-        private InputManagerBase inputManagerBase;
-
-        [SerializeField] private GameObject gameManager;
-
-        [SerializeField] private InputManagerType inputType;
-        public InputManagerBase InputManagerBase
-        {
-            get
-            {
-                if (!gameManager)
-                {
-                    gameManager = new GameObject("VR Manager");
-                    DontDestroyOnLoad(gameManager);
-                }
-
-                return CreateInputManager();
-            }
-        }
-        
-        private InputManagerBase CreateInputManager()
-        {
-            switch (inputType)
-            {
-                case InputManagerType.UnityAxisBased:
-                    if (inputManagerBase != null && inputManagerBase is AxisBasedInputManagerBase) return inputManagerBase;
-                    if (inputManagerBase) Destroy(inputManagerBase);
-                    inputManagerBase = gameManager.AddComponent<AxisBasedInputManagerBase>();
-                    break;
-                case InputManagerType.InputSystem:
-                    if (inputManagerBase != null && inputManagerBase is InputSystemBased) return inputManagerBase;
-                    if (inputManagerBase) Destroy(inputManagerBase);
-                    inputManagerBase = gameManager.AddComponent<InputSystemBased>();
-                    break;
-                case InputManagerType.KeyboardMock:
-                    if (inputManagerBase != null && inputManagerBase is KeyboardBasedInput) return inputManagerBase;
-                    if (inputManagerBase) Destroy(inputManagerBase);
-                    inputManagerBase = gameManager.AddComponent<KeyboardBasedInput>();
-
-                    break;
-            }
-
-            return inputManagerBase;
-        }
     }
     
     public class InputSystemBased : InputManagerBase
