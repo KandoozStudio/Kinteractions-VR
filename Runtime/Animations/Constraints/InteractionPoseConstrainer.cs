d48286d16bbde29f1e36b05a42dbdbb341f54b89
Ahmad Abobakr <Ahmadabobakr@gmail.com>
2023-08-11 19:34:22 +0400

    Renamed PoseConstrainter to Interaction Pose Constrainer to allow for other types of constrainers

--- Runtime/Animations/Constraints/InteractionPoseConstrainer.cs
+++ Runtime/Animations/Constraints/InteractionPoseConstrainer.cs
@@ -1,25 +1,45 @@
+using System;
 using Kandooz.InteractionSystem.Core;
+using Kinteractions_VR.Runtime.Animations.Constraints;
 using UnityEngine;
 
 namespace Kandooz.InteractionSystem.Interactions
 {
-    public class PoseConstrainter :MonoBehaviour
+    [RequireComponent(typeof(InteractableBase))]
+    public class InteractionPoseConstrainer : MonoBehaviour,IPoseConstrainer
     {
         [HideInInspector, SerializeField] private HandConstraints leftConstraints;
         [HideInInspector, SerializeField] private HandConstraints rightConstraints;
-        public Transform LeftHandPivot
+        private InteractableBase interactable;
+        public Transform LeftHandTransform
         {
             set => leftConstraints.relativeTransform = value;
             get => leftConstraints.relativeTransform;
         }
-        public Transform RightHandPivot
+        public Transform RightHandTransform
         {
             set => rightConstraints.relativeTransform = value;
             get => rightConstraints.relativeTransform;
         }
-
         public HandPoseConstraints LeftPoseConstraints => leftConstraints.poseConstraints;
         public HandPoseConstraints RightPoseConstraints => leftConstraints.poseConstraints;
+        
 
+        public void OnEnable()
+        {
+            interactable = GetComponent<InteractableBase>();
+            interactable.OnSelected += Constrain;
+            interactable.OnSelected += Unconstrain;
+        }
+
+        private void Unconstrain(InteractorBase arg0)
+        {
+            
+        }
+
+        private void Constrain(InteractorBase interactor)
+        {
+            
+        }
     }
 }
