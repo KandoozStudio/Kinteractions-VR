e3b49878aa49e01f423ce04cdacf002cec549120
Ahmad Abobakr <Ahmadabobakr@gmail.com>
2023-09-06 22:39:17 +0400

    Fixed finger mismatch issue

--- Runtime/Core/Input/AxisBasedInputManager.cs
+++ Runtime/Core/Input/AxisBasedInputManager.cs
@@ -4,28 +4,42 @@ namespace Kandooz.InteractionSystem.Core
 {
     internal class AxisBasedInputManager : InputManagerBase
     {
-        private const string LeftTriggerAxis = "XRI_Left_Trigger";
-        private const string LeftTriggerButton = "XRI_Left_TriggerButton";
-        private const string LeftGripAxis = "XRI_Left_Grip";
-        private const string LeftGripButton = "XRI_Left_GripButton";
+        private const string LEFT_TRIGGER_AXIS = "XRI_Left_Trigger";
+        private const string LEFT_TRIGGER_BUTTON = "XRI_Left_TriggerButton";
+        private const string LEFT_GRIP_AXIS = "XRI_Left_Grip";
+        private const string LEFT_GRIP_BUTTON = "XRI_Left_GripButton";
+        private const string XRI_LEFT_THUMB_TOUCH = "XRI_Left_ThumbTouch";
+        private const string XRI_LEFT_THUMBREST = "XRI_Left_Thumbrest";
+        private const string XRI_LEFT_PRIMARY_TOUCH = "XRI_Left_PrimaryTouch";
+        private const string XRI_LEFT_SECONDARY_TOUCH = "XRI_Left_SecondaryTouch";
 
-        private const string RightTriggerAxis = "XRI_Right_Trigger";
-        private const string RightTriggerButton = "XRI_Right_TriggerButton";
-        private const string RightGripAxis = "XRI_Right_Grip";
-        private const string RightGripButton = "XRI_Right_GripButton";
+        private const string RIGHT_TRIGGER_AXIS = "XRI_Right_Trigger";
+        private const string RIGHT_TRIGGER_BUTTON = "XRI_Right_TriggerButton";
+        private const string RIGHT_GRIP_AXIS = "XRI_Right_Grip";
+        private const string RIGHT_GRIP_BUTTON = "XRI_Right_GripButton";
+        private const string XRI_RIGHT_THUMB_TOUCH = "XRI_Right_ThumbTouch";
+        private const string XRI_RIGHT_THUMBREST = "XRI_Right_Thumbrest";
+        private const string XRI_RIGHT_PRIMARY_TOUCH = "XRI_Right_PrimaryTouch";
+        private const string XRI_RIGHT_SECONDARY_TOUCH = "XRI_Right_SecondaryTouch";
 
 
         private void Update()
         {
-            leftHand[1] = Input.GetAxisRaw(LeftTriggerAxis);
-            leftHand[2] = leftHand[3] = leftHand[4] = Input.GetAxisRaw(LeftGripAxis);
-            leftHand.triggerObserver.ButtonState = Input.GetButton(LeftTriggerButton);
-            leftHand.gripObserver.ButtonState = Input.GetButton(LeftGripButton);
+            var leftThumbPressed = Input.GetButton(XRI_LEFT_THUMB_TOUCH) || Input.GetButton(XRI_LEFT_THUMBREST) ||
+                                    Input.GetButton(XRI_LEFT_PRIMARY_TOUCH) || Input.GetButton(XRI_LEFT_SECONDARY_TOUCH);
 
-            rightHand[1] = Input.GetAxisRaw(RightTriggerAxis);
-            rightHand[2] = rightHand[3] = rightHand[4] = Input.GetAxisRaw(RightGripAxis);
-            rightHand.triggerObserver.ButtonState = Input.GetButton(RightTriggerButton);
-            rightHand.gripObserver.ButtonState = Input.GetButton(RightGripButton);
+            leftHand[0] = leftThumbPressed ? 1 : 0;
+            leftHand[1] = Input.GetAxisRaw(LEFT_TRIGGER_AXIS);
+            leftHand[2] = leftHand[3] = leftHand[4] = Input.GetAxisRaw(LEFT_GRIP_AXIS);
+            leftHand.triggerObserver.ButtonState = Input.GetButton(LEFT_TRIGGER_BUTTON);
+            leftHand.gripObserver.ButtonState = Input.GetButton(LEFT_GRIP_BUTTON);
+            var rightThumbPressed = Input.GetButton(XRI_RIGHT_THUMB_TOUCH) || Input.GetButton(XRI_RIGHT_THUMBREST) ||
+                                    Input.GetButton(XRI_RIGHT_PRIMARY_TOUCH) || Input.GetButton(XRI_RIGHT_SECONDARY_TOUCH);
+            rightHand[0] = rightThumbPressed ? 1 : 0;
+            rightHand[1] = Input.GetAxisRaw(RIGHT_TRIGGER_AXIS);
+            rightHand[2] = rightHand[3] = rightHand[4] = Input.GetAxisRaw(RIGHT_GRIP_AXIS);
+            rightHand.triggerObserver.ButtonState = Input.GetButton(RIGHT_TRIGGER_BUTTON);
+            rightHand.gripObserver.ButtonState = Input.GetButton(RIGHT_GRIP_BUTTON);
         }
     }
 }
