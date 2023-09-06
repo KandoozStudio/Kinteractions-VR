2688dc6019e8d29ebb4fba49a1a3549062527f14
Ahmad Abobakr <Ahmadabobakr@gmail.com>
2023-09-06 22:18:10 +0400

    Removed commented code

--- Runtime/Animations/Constraints/HandConstraints.cs
+++ Runtime/Animations/Constraints/HandConstraints.cs
@@ -7,18 +7,8 @@ namespace Kandooz.InteractionSystem.Core
     [System.Serializable]
     public struct HandConstraints
     {
-        //public HandPoseController prefab;
-        [FormerlySerializedAs("poseConstraints")] public PoseConstrains poseConstrains;
+        public PoseConstrains poseConstrains;
         public Transform relativeTransform;
-
-        // public void ConstraintHand(HandInputMapper mapper)
-        // {
-        //     mapper.Constraints = constraints;
-        // }
-        // public void UnConstraintHand(HandInputMapper mapper)
-        // {
-        //     mapper.Constraints = HandConstraints.Free;
-        // }
     }
 
 }
