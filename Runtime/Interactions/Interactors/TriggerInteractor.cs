bcf005edf4c794c03c8e578c1f70bc30b4312355
Ahmad Abobakr <Ahmadabobakr@gmail.com>
2023-09-08 11:43:09 +0400

    Trigger interactor now chooses the nearest collider.

--- Runtime/Interactions/Interactors/TriggerInteractor.cs
+++ Runtime/Interactions/Interactors/TriggerInteractor.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Kandooz.Interactions;
 using Kandooz.InteractionSystem.Core;
 using UnityEngine;
@@ -7,34 +9,61 @@ namespace Kandooz.InteractionSystem.Interactions
     public class TriggerInteractor : InteractorBase
     {
         [ReadOnly][SerializeField] private Collider currentCollider;
-
+        //private LinkedList<InteractableBase> availableInteractables;
+        private HashSet<InteractableBase> interactables = new(10);
+        private int frameCounter;
         private void OnTriggerEnter(Collider other)
         {
-            if (ShouldChangeInteractable(other, out var interactable)) return;
+            if (other == currentCollider || isInteracting) return;
+            var interactable = other.GetComponentInParent<InteractableBase>();
+            if (!interactable || interactables.Contains(interactable)) return;
+            interactables.Add(interactable);
+            if (!ShouldChangeInteractable( interactable)) return;
+            ChangeInteractable(other, interactable);
+        }
+
+        private void ChangeInteractable(Collider other, InteractableBase interactable)
+        {
             currentInteractable = interactable;
             currentCollider = other;
             OnHoverStart();
         }
 
-        private bool ShouldChangeInteractable(Collider other, out InteractableBase interactable )
+        private bool ShouldChangeInteractable(InteractableBase interactable )
         {
-            interactable = null;
-            if (currentCollider == other) return true;
-            if (currentInteractable is { CurrentState: InteractionState.Selected }) return true;
-
-            interactable = other.GetComponentInParent<InteractableBase>();
-            return interactable == null;
+            if (currentInteractable == null) return true;
+            var interactableDistance = (transform.position - interactable.transform.position).sqrMagnitude;
+            var currentInteractableDistance = (transform.position - currentInteractable.transform.position).sqrMagnitude;
+            return currentInteractableDistance > interactableDistance;
         }
 
         private void OnTriggerExit(Collider other)
         {
+            var interactable = other.GetComponent<InteractableBase>();
+            if (interactables.Contains(interactable)) interactables.Remove(interactable);
             
-            if( currentInteractable!=null  && currentInteractable.CurrentState== InteractionState.Selected) return;
+            if (currentInteractable != null && currentInteractable.CurrentState == InteractionState.Selected) return;
             if (currentCollider == other)
             {
                 currentCollider = null;
                 OnHoverEnd();
             }
+        }
+
+        private void Update()
+        {
+            frameCounter++;
+            if (isInteracting) return;
+            if (frameCounter < 5) return;
+            frameCounter = 0;
+            var position = transform.position;
+            var distanceToCurrent = (currentInteractable.transform.position - position).sqrMagnitude;
+            foreach (var interactable in interactables)
+            {
+                var interactableDistance = (interactable.transform.position - position).sqrMagnitude;
+                if (interactableDistance < distanceToCurrent) ChangeInteractable(interactable.GetComponentInChildren<Collider>(), interactable);
+            }
+            
         }
     }
 }
