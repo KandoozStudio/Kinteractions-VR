0ea34c2a9d8015b88180f81377af98762822e66a
Ahmad Abobakr <Ahmadabobakr@gmail.com>
2023-09-26 20:27:41 +0400

    Added Material highligeter for hover feedback

---
+++ Runtime/Core/Effects/MaterialHighlighter.cs
@@ -0,0 +1,41 @@
+using Kandooz.InteractionSystem.Interactions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Kandooz.InteractionSystem.Core
+{
+    [RequireComponent(typeof(InteractableBase))]
+    public class MaterialHighlighter : MonoBehaviour
+    {
+        private Renderer[] renderers;
+        private Color []color;
+        private void Awake()
+        {
+            renderers = GetComponentsInChildren<MeshRenderer>();
+            for (int i = 0; i < renderers.Length; i++)
+            {
+                color[i] = renderers[i].material.color;
+
+            }
+            color= new Color[renderers.Length];
+            var interactable=GetComponent<InteractableBase>();
+            interactable.OnHoverStarted += (interactor) =>
+            {
+                for (int i = 0; i < renderers.Length; i++)
+                {
+                    renderers[i].material.color = color[i] * .3f;
+
+                }
+            };
+            interactable.OnHoverEnded += (interactor) =>
+            {
+                for (int i = 0; i < renderers.Length; i++)
+                {
+                    renderers[i].material.color = color[i];
+
+                }
+            };
+        }
+    }
+
+}
