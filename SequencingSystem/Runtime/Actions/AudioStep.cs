84231b613ca236c1eb94a5d2e10d5e826d5de082
Ahmad Abobakr <Ahmadabobakr@gmail.com>
2023-10-03 23:48:10 +0400

    Audiostep no longer uses EveryUpdate

--- SequencingSystem/Runtime/Actions/AudioStep.cs
+++ SequencingSystem/Runtime/Actions/AudioStep.cs
@@ -7,9 +7,9 @@ namespace Kandooz.Kuest
     [RequireComponent(typeof(StepEvenListener))]
     public class AudioStep : MonoBehaviour
     {
-        [SerializeField] private  AudioClip clip;
+        [SerializeField] private AudioClip clip;
         private AudioSource source;
-        private bool started=false;
+        private bool started = false;
         private StepEvenListener listener;
 
         private void OnEnable()
@@ -20,17 +20,15 @@ namespace Kandooz.Kuest
             source.clip = clip;
             listener = GetComponent<StepEvenListener>();
             listener.OnStarted.Do(_ => source.Play())
-                .SelectMany(_ => Observable.EveryUpdate())
-                .Where(_ => !source.isPlaying)
-                .Do(_ => listener.OnActionCompleted())
+                .Do(_ => started = true)
                 .Subscribe()
                 .AddTo(this);
-            listener.OnFinished.Do(_=>source.Stop()).Subscribe().AddTo(this);
+            listener.OnFinished.Do(_ => source.Stop()).Subscribe().AddTo(this);
         }
 
         private void Update()
         {
-            if(!started ||source.isPlaying)return;
+            if (!started || source.isPlaying) return;
             listener.OnActionCompleted();
             started = false;
         }
