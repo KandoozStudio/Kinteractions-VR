9cf834105e881641103f85928195903fe8efeffd
Ahmad Abobakr <Ahmadabobakr@gmail.com>
2023-10-04 20:46:46 +0400

    Added a timer action

--- SequencingSystem/Runtime/Actions/InsertionAction.cs
+++ SequencingSystem/Runtime/Actions/InsertionAction.cs
@@ -1,9 +1,44 @@
+using System;
+using Kandooz.InteractionSystem.Interactions;
 using UnityEngine;
 
 namespace Kandooz.Kuest
 {
+    [RequireComponent(typeof(StepEvenListener))]
     public class InsertionAction : MonoBehaviour
     {
-        
+        [SerializeField] private InteractableBase interactable;
+        private GameObject interactableObject;
+        private bool insideTrigger=false;
+        private StepEvenListener listner;
+        private void Awake()
+        {
+            listner = GetComponent<StepEvenListener>();
+            interactableObject = interactable.gameObject;
+            interactable.OnDeselected += OnSelectionEnded;
+
+        }
+
+        private void OnSelectionEnded(InteractorBase interactor)
+        {
+            if(!listner.Current || !insideTrigger) return;
+            interactableObject.transform.position = this.transform.position;
+            interactableObject.transform.rotation = this.transform.rotation;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject == interactableObject)
+            {
+                insideTrigger = true;
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject == interactableObject)
+            {
+                insideTrigger = false;
+            }
+        }
     }
 }
---
+++ SequencingSystem/Runtime/Actions/TimerAction.cs
@@ -0,0 +1,31 @@
+
+using System;
+using Kandooz.InteractionSystem.Core;
+using UnityEngine;
+
+namespace Kandooz.Kuest
+{
+    [RequireComponent(typeof(StepEvenListener))]
+    public class TimerAction : MonoBehaviour
+    {
+        [SerializeField] private float time;
+        [ReadOnly] private float elapsed=0;
+        private StepEvenListener listener;
+
+        private void Awake()
+        {
+            listener = GetComponent<StepEvenListener>();
+        }
+
+        private void Update()
+        {
+            if(!listener.Current) return;
+            elapsed += Time.deltaTime;
+            if (elapsed >= time)
+            {
+                elapsed = 0;
+                listener.OnActionCompleted();
+            }
+        }
+    }
+}
