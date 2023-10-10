7625e28496005bd0c51210dc897680abec3b9fc2
Ahmad Abobakr <Ahmadabobakr@gmail.com>
2023-10-10 18:14:50 +0400

    refactored Hand script and added a method for toggling the hand model

--- InteractionSystem/Runtime/Core/Hand/Hand.cs
+++ InteractionSystem/Runtime/Core/Hand/Hand.cs
@@ -1,6 +1,7 @@
 using System;
 using Kandooz.InteractionSystem.Animations.Constraints;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Kandooz.InteractionSystem.Core
 {
@@ -8,24 +9,29 @@ namespace Kandooz.InteractionSystem.Core
     {
         [SerializeField] private HandIdentifier hand;
         [SerializeField] private Config config;
+        [SerializeField] private Renderer handModel;
+        private IPoseable poseDriver;
+        
         public HandIdentifier HandIdentifier
         {
             get => hand;
             internal set => hand = value;
         }
-        internal Config Config
-        {
-            set => config = value;
-        }
         public IObservable<ButtonState> OnTriggerTriggerButtonStateChange => config.InputManager[hand].TriggerObservable ;
         public IObservable<ButtonState> OnGripButtonStateChange =>  config.InputManager[hand].GripObservable;
         public float this[FingerName index] => config.InputManager[hand][(int)index];
         public float this[int index] => config.InputManager[hand][index];
-        private IPoseable poseDriver;
+        internal Config Config
+        {
+            set => config = value;
+        }
+
+        public void ToggleRenderer(bool enable) => handModel.enabled = enable;
 
         private void Awake()
         {
             poseDriver = GetComponent<IPoseable>();
+            if (handModel == null) handModel = GetComponentInChildren<MeshRenderer>();
         }
 
         public void Constrain(IPoseConstrainer constrainer)
