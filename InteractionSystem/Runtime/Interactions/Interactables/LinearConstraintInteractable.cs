f285e07258651c7ee63fa05861a03d4d61121eeb
Ahmad Abobakr <Ahmadabobakr@gmail.com>
2023-10-12 22:57:37 +0400

    refactored the linear constrained interactable and made it lerp to position instead of snapping

--- InteractionSystem/Runtime/Interactions/Interactables/LinearConstraintInteractable.cs
+++ InteractionSystem/Runtime/Interactions/Interactables/LinearConstraintInteractable.cs
@@ -6,7 +6,7 @@ namespace Kandooz.InteractionSystem.Interactions
 {
     public class LinearConstraintInteractable : InteractableBase
     {
-        [SerializeField] private VariableTweener tweener;
+        //todo: editor
         [SerializeField] private Transform point1;
         [SerializeField] private Transform point2;
         [SerializeField] private Transform interactableObject;
@@ -14,18 +14,18 @@ namespace Kandooz.InteractionSystem.Interactions
         [SerializeField] private HandPoseController rightHand;
         private (Vector3 position, Quaternion rotation) leftHandPivot;
         private (Vector3 position, Quaternion rotation) rightHandPivot;
-        private Transform hand;
-
+        private (Vector3 position, Quaternion rotation) currentPivot;
+        private Transform fakeHandTransform;
+        private float t;
         protected override void Select()
         {
                 CurrentInteractor.ToggleHandModel(false);
-                hand = (CurrentInteractor.HandIdentifier == HandIdentifier.Left ? leftHand : rightHand).transform;
-                hand.gameObject.SetActive(true);
-                
-                // Move to same position as Hand
-                // Lerp the object to nearest possible position
-                //Lerp the hand to local position and rotation of pivot
-
+                fakeHandTransform = (CurrentInteractor.HandIdentifier == HandIdentifier.Left ? leftHand : rightHand).transform;
+                currentPivot = (CurrentInteractor.HandIdentifier == HandIdentifier.Left ? leftHandPivot : rightHandPivot);
+                fakeHandTransform.position = CurrentInteractor.transform.position;
+                fakeHandTransform.rotation = CurrentInteractor.transform.rotation;
+                fakeHandTransform.gameObject.SetActive(true);
+                t = 0;
         }
 
         protected override void DeSelected()
@@ -42,36 +42,32 @@ namespace Kandooz.InteractionSystem.Interactions
         protected override void StartHover()
         {
         }
-
-        protected override void EndHover()
-        {
-        }
-
-        private void Update()
-        {
-            if(!IsSelected)return;
-            var position2= point2.position;
-            var position1 = point1.position;
-            var direction = (position2 - position1);
-            var projectedPoint = Vector3.Project(CurrentInteractor.transform.position - position1, direction) + position1;
-            var t = FindNormalizedDistanceAlongPath(direction, projectedPoint, position1);
-            projectedPoint = Vector3.Lerp(position1, position2, t);
-            interactableObject.transform.position = projectedPoint;
-        }
-
-
+        protected override void EndHover(){}
         private void Awake()
         {
             leftHandPivot = new()
             {
-                position = leftHand.transform.position,
-                rotation = leftHand.transform.rotation
+                position = leftHand.transform.localPosition
+                ,
+                rotation = leftHand.transform.localRotation
             };
             rightHandPivot = new()
             {
-                position = rightHand.transform.position,
-                rotation = rightHand.transform.rotation
+                position = rightHand.transform.localPosition,
+                rotation = rightHand.transform.localRotation
             };
+        }
+
+        private void Update()
+        {
+            if (!IsSelected) return;
+            interactableObject.transform.position = 
+                GetPositionBetweenTwoPoints(
+                    CurrentInteractor.transform.position,
+                    point1.position,
+                    point2.position
+                    );
+            SetInteractionHandPosition();
         }
 
         private void OnDrawGizmos()
@@ -79,7 +75,6 @@ namespace Kandooz.InteractionSystem.Interactions
             var position2= point2.position;
             var position1 = point1.position;
             var direction = (position2 - position1);
-
             Gizmos.color = Color.red;
             Gizmos.DrawSphere(position2,.03f);
             Gizmos.DrawSphere(position1,.03f);
@@ -89,13 +84,28 @@ namespace Kandooz.InteractionSystem.Interactions
             Gizmos.DrawLine(position2, position1);
             Gizmos.color=Color.blue;
             Gizmos.DrawLine(projectedPoint, interactableObject.position);
-            var t = FindNormalizedDistanceAlongPath(direction, projectedPoint, position1);
-            projectedPoint = Vector3.Lerp(position1, position2, t);
-
+            projectedPoint = GetPositionBetweenTwoPoints(projectedPoint,position1,position2);
             Gizmos.color = Color.yellow;
             Gizmos.DrawSphere(projectedPoint,.03f);
 
         }
+        private void SetInteractionHandPosition()
+        {
+            t += Time.deltaTime * 10;
+            fakeHandTransform.localPosition = Vector3.Lerp(fakeHandTransform.localPosition, currentPivot.position, t);
+            fakeHandTransform.localRotation = Quaternion.Lerp(fakeHandTransform.localRotation, currentPivot.rotation, t);
+
+
+        }
+
+        private static Vector3 GetPositionBetweenTwoPoints(Vector3 point,Vector3 start,Vector3 end)
+        {
+            var direction = (end - start);
+            var projectedPoint = Vector3.Project(point - start, direction) + start;
+            var normalizedDistance = FindNormalizedDistanceAlongPath(direction, projectedPoint, start);
+            return Vector3.Lerp(start, end, normalizedDistance);
+        }
+
 
         private static float FindNormalizedDistanceAlongPath(Vector3 direction, Vector3 projectedPoint, Vector3 position1)
         {
